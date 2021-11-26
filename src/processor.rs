057231fabb4215e9586978ac522d4679f496e171
Filip Parag <filip@parag.rs>
2021-11-27 00:10:05 +0100

    stream digest

--- src/processor.rs
+++ src/processor.rs
@@ -86,6 +86,38 @@ impl StreamProcessor {
         }
     }
 
+    fn digest(&mut self) -> Result<(), Error> {
+        match str::from_utf8(&self.buffer_consumed[..self.consumed]) {
+            Ok(str) => {
+                let chars = str.chars();
+                let count = chars.count();
+                if self.digested + count >= DIGESTED_BUFFER {
+                    return Err(Error::BufferOverflow);
+                }
+                for (i, c) in str.chars().enumerate() {
+                    self.buffer_digested[self.digested + i] = c;
+                }
+                self.digested += count;
+                self.consumed = 0;
+                Ok(())
+            }
+            Err(e) => {
+                if e.valid_up_to() > 0 {
+                    let remaining = self.consumed - e.valid_up_to();
+                    self.consumed = e.valid_up_to();
+                    self.digest()?;
+                    self.consumed = remaining;
+                    for i in 0..remaining {
+                        self.buffer_consumed[i] = self.buffer_consumed[i + e.valid_up_to()];
+                    }
+                    Ok(())
+                } else {
+                    Err(Error::UTFError(e))
+                }
+            }
+        }
+    }
+
 }
 
 #[cfg(test)]
@@ -142,8 +174,7 @@ mod tests {
             let mut consumed: usize = 0;
             while consumed < EXAMPLES[e].0.as_bytes().len() {
                 match proc.consume(&EXAMPLES[e].0.as_bytes()[consumed..]) {
-                    Ok(v) => {
-                        consumed += v;
+                    Ok(_) => {
                         break;
                     }
                     Err(v) => {
@@ -155,8 +186,62 @@ mod tests {
                         proc.buffer_consumed = [0u8; CONSUMED_BUFFER];
                     }
                 }
-}
+            }
         }
         Ok(())
     }
+
+    #[test]
+    fn test_digest() -> Result<(), Error> {
+        for (_, e) in EXAMPLES {
+            let mut proc = StreamProcessor::new(Direction::LatToCyr);
+            let mut chars: Vec<char> = vec![];
+            e.chars().for_each(|c| chars.push(c));
+            if chars.len() < DIGESTED_BUFFER / 3 {
+                continue;
+            }
+            // Fill up a part of digested buffer
+            proc.buffer_digested[..DIGESTED_BUFFER / 3]
+                .copy_from_slice(&chars[..DIGESTED_BUFFER / 3]);
+            proc.digested = DIGESTED_BUFFER / 3;
+            let mut remaining_char: Vec<u8> = vec![];
+            for c in &chars[DIGESTED_BUFFER / 3..] {
+                let mut b = c.to_string().as_bytes().to_vec();
+                if b.len() > 1 && proc.consumed >= CONSUMED_BUFFER - 5 {
+                    // Intentionally break last character
+                    proc.buffer_consumed[proc.consumed] = b[0];
+                    proc.consumed += 1;
+                    b.remove(0);
+                    remaining_char.append(&mut b);
+                    break;
+                }
+                proc.buffer_consumed[proc.consumed..proc.consumed + b.len()].copy_from_slice(&b);
+                proc.consumed += b.len();
+                if proc.consumed >= CONSUMED_BUFFER {
+                    break;
+                }
+            }
+            proc.digest()?;
+            let mut d = proc.digested;
+            assert_eq!(
+                proc.buffer_digested[..proc.digested],
+                chars[..proc.digested]
+            );
+            if remaining_char.is_empty() {
+                continue;
+            }
+            // Check if last caracter gets stitched
+            proc.buffer_consumed[proc.consumed..proc.consumed + remaining_char.len()]
+                .copy_from_slice(&remaining_char);
+            proc.consumed += remaining_char.len();
+            proc.digest()?;
+            assert!(proc.digested == d + 1);
+            assert_eq!(
+                proc.buffer_digested[..proc.digested],
+                chars[..proc.digested]
+            );
+        }
+        Ok(())
+    }
+
 }
